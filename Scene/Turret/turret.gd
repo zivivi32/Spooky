87053c7f144ccc8052ecf3656ff9340440606403
Olivier Mint <oliviercorimbelly@gmail.com>
2024-10-18 23:33:37 +0200

    changed visuals for turret

--- Scene/Turret/turret.gd
+++ Scene/Turret/turret.gd
@@ -8,6 +8,7 @@ class_name Turret
 @export var timer: Timer
 
 @export_subgroup("VFX")
+@export var move_vfx_particles: Array[GPUParticles3D]
 @export var spawn_particles: GPUParticles3D
 @export var death_particles: GPUParticles3D
 
@@ -16,7 +17,8 @@ var target: Enemy
 func _ready() -> void:
 	timer.start(turret_lifetime)
 	timer.connect("timeout", die)
-	
+	model.scale = Vector3(1.2 ,1.7 ,1.2)
+	play_move_vfx()
 	if spawn_particles:
 		spawn_particles.emitting = true
 
@@ -28,9 +30,22 @@ func die():
 		await  death_particles.finished
 	queue_free()
 
+func _process(delta: float) -> void:
+	model.scale = model.scale.lerp(Vector3(1, 1, 1), delta * 10)
+
+
+func play_move_vfx(): 
+	if move_vfx_particles: 
+		for particles in move_vfx_particles:
+			particles.restart()
+			particles.emitting = true
+
 func _physics_process(_delta: float) -> void:
 	var targets = detection.get_overlapping_bodies()
 	if targets.size() > 0: 
+		if target != targets.front():
+			model.scale = Vector3(1.0 ,1.75,1.0)
+			play_move_vfx()
 		target = targets.front()
 		if gun.attack_timer.is_stopped():
 			gun.attack_timer.start()
@@ -43,6 +58,6 @@ func update_gun_aim(target_position : Vector3):
 		return  # Don't rotate if no valid target
 
 	target_position.y = 0
-	gun.look_at(target_position, Vector3.UP, true)
-	gun.rotation.x = 0
-	gun.rotation.z = 0
+	model.look_at(target_position, Vector3.UP, true)
+	model.rotation.x = 0
+	model.rotation.z = 0
