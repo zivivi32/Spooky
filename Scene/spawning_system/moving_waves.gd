ada80c2bf3abe4b16d9625fa53c64594588cc716
Olivier Mint <oliviercorimbelly@gmail.com>
2024-11-17 21:25:31 +0100

    Spawning ghost and ghost boss remaining.

--- Scene/spawning_system/moving_waves.gd
+++ Scene/spawning_system/moving_waves.gd
@@ -1,6 +1,9 @@
 extends PathFollow3D
 
 @export var speed = 0.1
+@export var easy_wave_configurations: WaveData
+@export var medium_wave_configurations: WaveData
+@export var hard_wave_configurations: WaveData
 signal restart
 
 var is_wave_comlete : bool = false
@@ -27,3 +30,18 @@ func _on_restart() -> void:
 		$Invaders_Spawner.start_next_wave()
 		is_wave_comlete = false
 		
+
+
+func _on_enemy_spawn_easy_wave_signal() -> void:
+	##Easy Mode
+	$Invaders_Spawner.waves.clear()
+	$Invaders_Spawner.waves.append(easy_wave_configurations)
+
+func _on_enemy_spawn_hard_wave_signal() -> void:
+	$Invaders_Spawner.waves.clear()
+	$Invaders_Spawner.waves.append(easy_wave_configurations)
+
+
+func _on_enemy_spawn_medium_wave_signal() -> void:
+	$Invaders_Spawner.waves.clear()
+	$Invaders_Spawner.waves.append(easy_wave_configurations)
